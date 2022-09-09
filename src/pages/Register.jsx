460546b0fc7a1c5d0d75ee20ef639848e5519525
strong-minsu <dlalstn5789@gmail.com>
2022-09-09 23:19:03 +0900

    register 수정

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -32,10 +32,13 @@ const tailFormItemLayout = {
     },
   },
 };
-
-const style = {
+// const style1 = {
+//   width: "510px",
+//   height: "720px",
+// };
+const style2 = {
   padding: "20px",
-  width: "500px",
+  width: "430px",
   border: "solid 2px #1890ff",
 };
 const sort = {
@@ -46,18 +49,9 @@ const sort = {
 };
 
 const Register = () => {
-  // const [popup, setPopup] = useState(false);
   const [form] = Form.useForm();
   let navigate = useNavigate();
   const Finish = (values) => {
-    // console.log("Received values of form: ", values);
-    // fetch("http://192.168.56.1:8080/register", {
-    //   method: "POST",
-    //   body: JSON.stringify(values),
-    // })
-    //   .then((response) => response.json())
-    //   .then((result) => console.log("결과: ", result));
-
     const data = {
       userId: values.nickname,
       password: values.password,
@@ -78,9 +72,10 @@ const Register = () => {
       })
       .catch(function (error) {
         console.log("오류!");
-        console.log(JSON.stringify(data));
+        // console.log(JSON.stringify(data));
       });
 
+    //fetch로 보내기
     // fetch("/register", {
     //   method: "post",
     //   body: JSON.stringify(data),
@@ -96,129 +91,134 @@ const Register = () => {
     //   // 현재 페이지 새로고침
     //   window.location.reload();
     // });
+
     navigate("/login");
   };
 
   return (
-    <div style={sort}>
-      <Form
-        {...formItemLayout}
-        form={form}
-        name="register"
-        onFinish={Finish}
-        scrollToFirstError
-        style={style}
-        autoComplete="off"
-      >
-        <Form.Item
-          name="email"
-          label="E-mail"
-          rules={[
-            {
-              type: "email",
-              message: "The input is not valid E-mail!",
-            },
-            {
-              required: true,
-              message: "Please input your E-mail!",
-            },
-          ]}
+    <div>
+      <div style={sort}>
+        <Form
+          {...formItemLayout}
+          form={form}
+          name="register"
+          onFinish={Finish}
+          scrollToFirstError
+          style={style2}
+          autoComplete="off"
         >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          name="password"
-          label="Password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-          ]}
-          hasFeedback
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item
-          name="confirm"
-          label="Confirm Password"
-          dependencies={["password"]}
-          hasFeedback
-          rules={[
-            {
-              required: true,
-              message: "Please confirm your password!",
-            },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue("password") === value) {
-                  return Promise.resolve();
-                }
-
-                return Promise.reject(
-                  new Error("The two passwords that you entered do not match!")
-                );
+          <Form.Item
+            name="email"
+            label="E-mail"
+            rules={[
+              {
+                type: "email",
+                message: "The input is not valid E-mail!",
               },
-            }),
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+              {
+                required: true,
+                message: "Please input your E-mail!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item
-          name="nickname"
-          label="Nickname"
-          tooltip="What do you want others to call you?"
-          rules={[
-            {
-              required: true,
-              message: "Please input your nickname!",
-              whitespace: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item
-          name="intro"
-          label="Intro"
-          rules={[
-            {
-              required: true,
-              message: "Please input Intro",
-            },
-          ]}
-        >
-          <Input.TextArea showCount maxLength={100} />
-        </Form.Item>
+          <Form.Item
+            name="confirm"
+            label="Confirm Password"
+            dependencies={["password"]}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: "Please confirm your password!",
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
 
-        <Form.Item
-          name="agreement"
-          valuePropName="checked"
-          rules={[
-            {
-              validator: (_, value) =>
-                value
-                  ? Promise.resolve()
-                  : Promise.reject(new Error("Should accept agreement")),
-            },
-          ]}
-          {...tailFormItemLayout}
-        >
-          {/* 주소 수정하기 */}
-          <Checkbox>
-            I have read the <a href="/">agreement</a>
-          </Checkbox>
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Register
-          </Button>
-        </Form.Item>
-      </Form>
+                  return Promise.reject(
+                    new Error(
+                      "The two passwords that you entered do not match!"
+                    )
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="nickname"
+            label="Nickname"
+            tooltip="What do you want others to call you?"
+            rules={[
+              {
+                required: true,
+                message: "Please input your nickname!",
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="intro"
+            label="Intro"
+            rules={[
+              {
+                required: true,
+                message: "Please input Intro",
+              },
+            ]}
+          >
+            <Input.TextArea />
+          </Form.Item>
+
+          <Form.Item
+            name="agreement"
+            valuePropName="checked"
+            rules={[
+              {
+                validator: (_, value) =>
+                  value
+                    ? Promise.resolve()
+                    : Promise.reject(new Error("Should accept agreement")),
+              },
+            ]}
+            {...tailFormItemLayout}
+          >
+            {/* 주소 수정하기 */}
+            <Checkbox>
+              I have read the <a href="/">agreement</a>
+            </Checkbox>
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
